43613bfb4315c0d4fd76a663c3da864f4c64fca8
vavladim <vangelis.vladimirov@cern.ch>
2023-09-30 20:38:40 +0200

    HIExpressRawPrime_374289

--- HeavyIonsAnalysis/Configuration/test/crab_many_files.py
+++ HeavyIonsAnalysis/Configuration/test/crab_many_files.py
@@ -3,8 +3,8 @@ from CRABClient.UserUtilities import getUsername
 
 config = Configuration()
 
-inputList = 'HIExpress_374289_files.txt'
-jobTag = "HIExpress_374289"
+inputList = 'HIExpressRawPrime_374289_files.txt'
+jobTag = "HIExpressRawPrime_374289"
 username = getUsername()
 
 config.section_("General")
